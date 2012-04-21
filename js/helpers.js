1668918a41338fef2b700a3e8d8e10752c769d6a
mildmojo <timk@apple-iie.local>
2012-04-21 15:11:08 -0400

    Initial commit, added JS libs, placeholder gfx, minor game setup code

---
+++ js/helpers.js
@@ -0,0 +1,9 @@
+// helpers.js
+
+function center_in_x(val) {
+  return SCREEN_WIDTH / 2.0 - val / 2.0;
+}
+
+function center_in_y(val) {
+  return SCREEN_HEIGHT / 2.0 - val / 2.0;
+}
---
+++ js/main.js
@@ -0,0 +1,44 @@
+var SCREEN_WIDTH;
+var SCREEN_HEIGHT;
+
+$(function(){
+
+  // FULL SCREEN LIKE A BOSS
+  Crafty.init();
+
+  // Monitor browser resize events, like mobile orientation changes
+  $.resize(function(){
+    SCREEN_WIDTH  = Crafty.DOM.window.width;
+    SCREEN_HEIGHT = Crafty.DOM.window.height;
+  });
+
+
+  Crafty.scene('loading', function(){
+    Crafty.background( '#333' );
+
+    msg = Crafty.e( '2D, DOM, Text, loading-text' )
+      .text( 'Circumnavigating, please wait...' )
+      .css({ size: SCREEN_HEIGHT / 5 });
+
+    msg.attr({ x: center_in_x(msg.width), y: -msg.height - 10 });
+
+    $(msg._element).animate({ top: center_in_y( msg.height ) });
+
+    var assets = [
+      'images/unicycles.png'
+      ,'images/worlds.png'
+      ,'images/starfield.png'
+    ]
+
+    Crafty.load(assets, function(){
+      Crafty.scene('main');
+    })
+
+  });
+
+
+
+
+  Crafty.scene('loading');
+
+});
